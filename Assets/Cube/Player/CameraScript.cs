c0fe2b8419194edc2b832f59bc59a2db983ec53c
Maxim <maxim.kosterov@gmail.com>
2020-05-28 22:49:45 +0300

    added camera follow

---
+++ Assets/Cube/Player/CameraScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vars;
+
+public class CameraScript : MonoBehaviour
+{
+public GameObject player;
+
+private Vector3 offset;
+
+
+// Start is called before the first frame update
+void Start()
+{
+	offset = transform.position - player.transform.position;
+}
+
+// Update is called once per frame
+void Update()
+{
+	transform.position = Vector3.Lerp(transform.position, player.transform.position+offset, Statics.smoothing*Time.deltaTime);
+}
+}
