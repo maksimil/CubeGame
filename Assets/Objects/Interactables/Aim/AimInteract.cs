ad4b714ec0fea303376ed1344ef37f2849723066
Maxim <maxim.kosterov@gmail.com>
2020-05-30 20:43:25 +0300

    Added particle on aim

---
+++ Assets/Objects/Interactables/Aim/AimInteract.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cube;
+
+namespace Interactables
+{
+public class AimInteract : MonoBehaviour, Interactable
+{
+public void Interact(MainCubeScript player)
+{
+	Debug.Log("Next lvl");
+}
+}
+}
